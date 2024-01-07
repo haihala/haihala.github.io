15bb556683a30ebaa2da743290b470edc1b3a31c
Eero H??ih??l?? <eero.haihala@gmail.com>
2024-01-07 22:56:54 +0200

    Fix article gathering

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,4 +1,5 @@
-import type { PageLoad } from './$types';
+import { load_pages } from '$lib/load_posts';
+import type { EntryGenerator, PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params }) => {
 	const post = await import(`../../../lib/posts/${params.slug}.md`);
@@ -6,4 +7,14 @@ export const load: PageLoad = async ({ params }) => {
 	return {
 		content: post.default
 	};
+};
+
+export const entries: EntryGenerator = async () => {
+	const pages = await load_pages();
+
+	return pages.map((page) => {
+		return {
+			slug: page.link.replace('/blog/', '')
+		};
+	});
 };
