4eb5cdb38caf81c8fbf4c645389373d350c2f64f
Eero Häihälä <eero.haihala@gmail.com>
2024-12-13 16:49:26 +0200

    Redo about page
    Add bluesky link and icons

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,40 @@
+<script lang="ts">
+	type Social = {
+		link: string;
+		image: string;
+		text: string;
+	};
+	const socials: Social[] = [
+		{
+			link: 'https://github.com/haihala',
+			text: 'Github',
+			image: 'https://cdn-icons-png.flaticon.com/512/25/25231.png'
+		},
+
+		{
+			link: 'https://bsky.app/profile/haihala.bsky.social',
+			text: 'Bluesky',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Bluesky_Logo.svg/1159px-Bluesky_Logo.svg.png'
+		},
+		{
+			link: 'https://www.linkedin.com/in/eero-haihala/',
+			text: 'Linkedin',
+			image: 'https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png'
+		},
+		{
+			link: 'https://hajhawa.itch.io/',
+			text: 'Itch.io',
+			image: 'https://static-00.iconduck.com/assets.00/itch-io-icon-2048x2048-i6hzclad.png'
+		},
+		{
+			link: 'mailto:eero.haihala@gmail.com',
+			text: 'Email',
+			image: 'https://pngimg.com/d/email_PNG11.png'
+		}
+	];
+</script>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="About me" />
@@ -14,10 +51,32 @@
 		<a href="/blog">my blog</a>, althought I make no promises.
 	</p>
 
-	<p>
-		You can find me on <a href="https://github.com/haihala">GitHub</a>,
-		<a href="https://www.linkedin.com/in/eero-haihala/">linkedin</a>,
-		<a href="https://hajhawa.itch.io/">itch.io</a>, or reach out by email at
-		<a href="mailto:eero.haihala@gmail.com">eero.haihala@gmail.com</a>
-	</p>
+	<div class="socials">
+		You can get in touch via:
+		{#each socials as social}
+			<a href={social.link}>
+				<img src={social.image} alt="{social.text} logo" />
+				{social.text}
+			</a>
+		{/each}
+	</div>
 </section>
+
+<style>
+	img {
+		height: 1.5rem;
+	}
+
+	div.socials {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: center;
+
+		& > a {
+			padding-left: 1rem;
+			display: flex;
+			gap: 0.3rem;
+			align-items: center;
+		}
+	}
+</style>
